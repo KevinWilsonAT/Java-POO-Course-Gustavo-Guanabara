67d5939710664906ac9a3237a8f12fd1cd3b5c1c
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-25 20:00:49 -0300

    chore: Update classes 25-03-24

---
+++ Java POO/Practice/Class03/src/main/java/com/cursoemvideo/class03/Class03.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.cursoemvideo.class03;
+
+/**
+ *
+ * @author kevin
+ */
+public class Class03 {
+
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.model = "BIC";
+        //p1.tip = 0.8f;
+        p1.color = "Blue";
+        p1.charge = 80;
+        
+        p1.uncover();
+        p1.status();
+        p1.draw();
+    }
+}
---
+++ Java POO/Practice/Class03/src/main/java/com/cursoemvideo/class03/Pen.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cursoemvideo.class03;
+
+/**
+ *
+ * @author kevin
+ */
+public class Pen {
+    public String model;
+    public String color;
+    private Float tip;
+    protected int charge;
+    protected boolean covered;
+    
+    void status(){
+        System.out.println("Pen Model: " + this.model);
+        System.out.println("Pen Color: " + this.color);
+        System.out.println("Pen Tip Width: " + this.tip);
+        System.out.println("Pen Charge (%): " + this.charge);
+        System.out.println("Pen covered? " + this.covered);
+    }
+    
+    public void draw(){
+        if(this.covered == true){
+            System.out.println("ERROR! Cannot draw with Pen Covered");
+        }
+        else{
+            System.out.println("Pen Drawing...");
+        }
+    }
+    
+    protected void cover(){
+        this.covered = true;
+    }
+    
+    protected void uncover(){
+        this.covered = false;
+    }
+}
+
