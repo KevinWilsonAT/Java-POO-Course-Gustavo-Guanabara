2cd30014e1dadb80ff188073bc2a37d12702dd5d
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-04-17 21:51:29 -0300

    chore: upload classes 17-04-24

---
+++ Java POO/Practice/Class07Fighting/src/main/java/com/cursoemvideo/class07fighting/Class07Fighting.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.cursoemvideo.class07fighting;
+
+/**
+ *
+ * @author kevin
+ */
+public class Class07Fighting {
+
+    public static void main(String[] args) {
+        Fighter f = new Fighter("Player1", "France", 31, 2.00f, 65.0f, 11, 2, 1);
+        
+        f.display();
+    }
+}
